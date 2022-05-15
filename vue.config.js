2e4a89c0dba4fcac13232f577b875bef301335cf
Markus Haas <haas.markus@protonmail.com>
2022-05-15 18:07:02 +0200

    Made it work; Temporarily a google cloud link, lets change to ipfs

--- vue.config.js
+++ vue.config.js
@@ -13,4 +13,11 @@ module.exports = {
             return args;
         });
     },
+    devServer: {
+        headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+            "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
+        }
+    },
 };
